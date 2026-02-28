47f94e0b9c58ca8584c0c5815323a16262e72fcc
Leo Gharbaruk <ghabaruk@gmail.com>
2026-02-28 15:36:18 +0300

    v1 almost ready

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 const startBtn = document.querySelector('.start'),
     left = document.querySelector('.left'),
     right = document.querySelector('.right'),
-    reloadBtn = document.querySelector('.reload');
+    sidesArray = [left, right];
+reloadBtn = document.querySelector('.reload');
 
-const ship = document.querySelector('.spacecraft'),
+const ship = document.querySelector('.ship-container'),
     body = document.querySelector('body'),
     content = document.querySelector('.controls'),
     flames = document.querySelector('.flames'),
     flameArray = Array.from(document.querySelectorAll('.flame'));
 
+document.addEventListener('DOMContentLoaded', () => {
+    if (isMobile) {
+        sidesArray.forEach(btn => btn.classList.add('mobile'));
+    };
+})
 startBtn.addEventListener('click', (event) => {
     event.stopPropagation();
     flames.classList.add('flames-visible');
@@ -17,13 +25,26 @@ startBtn.addEventListener('click', (event) => {
 
 let difficultySpeed = 1.5,
     sideDistance = 0,
-    upDistance = 0,
     isPlaying = false;
 
 function startGame() {
     isPlaying = true;
+    createAsteroid();
+    setTimeout(() => {
+        createAsteroid();
+    }, 400);
+    setTimeout(() => {
+        createAsteroid();
+    }, 55);
+    setTimeout(() => {
+        createAsteroid();
+    }, 11);
+}
+
+function createAsteroid() {
     setInterval(() => {
         let newAsteroid = document.createElement('img');
+        newAsteroid.style.left = (Math.random() * 97) + '%';
         newAsteroid.src = './img/asteroid.webp';
         newAsteroid.className = 'asteroid';
         body.appendChild(newAsteroid);
@@ -33,58 +54,39 @@ function startGame() {
             newAsteroid.style.top = asteroidMargin + 'px';
         }, 0.1);
     }, 945);
-    setTimeout(() => {
-        setInterval(() => {
-            let newAsteroid = document.createElement('img');
-            newAsteroid.src = './img/asteroid.webp';
-            newAsteroid.className = 'otherAsteroid';
-            body.appendChild(newAsteroid);
-            let asteroidMargin = 0;
-            setInterval(() => {
-                asteroidMargin += difficultySpeed;
-                newAsteroid.style.top = asteroidMargin + 'px';
-            }, 0.1);
-        }, 945);
-    }, 200);
 }
 
-left.addEventListener('mousedown', (event) => {
-    event.stopPropagation();
-    if (isPlaying) {
-        leftInterval = setInterval(() => {
-            sideDistance += 11;
-            upDistance += 0.5;
-            ship.style.bottom = upDistance + 'px';
-            flameArray.forEach((flame) => {
-                flame.style.marginTop = -(upDistance + 3) + 'px';
-                flame.style.marginRight = 2 * sideDistance + 'px';
-            })
-            ship.style.right = sideDistance + 'px';
-        }, 100)
-    }
-})
-
-left.addEventListener('mouseup', () => {
-    clearInterval(leftInterval);
-})
-
 right.addEventListener('mousedown', rightStep);
+left.addEventListener('mousedown', leftStep);
 
 function rightStep() {
     if (isPlaying) {
         sideDistance -= 3;
-        upDistance -= 0.5;
-        ship.style.bottom = upDistance + 'px';
-        flames.style.marginTop = -(upDistance + 3) + 'px';
-        flames.style.marginRight = 2 * sideDistance + 'px';
-        ship.style.right = sideDistance + 'px';
+        ship.style.marginRight = sideDistance + 'px';
         RightStepStopID = requestAnimationFrame(rightStep);
     }
-
-}
+    else {
+        return;
+    };
+};
 
 right.addEventListener('mouseup', () => {
     cancelAnimationFrame(RightStepStopID);
+})
+
+function leftStep() {
+    if (isPlaying) {
+        sideDistance += 3;
+        ship.style.marginRight = sideDistance + 'px';
+        LeftStepStopID = requestAnimationFrame(leftStep);
+    }
+    else {
+        return;
+    };
+};
+
+left.addEventListener('mouseup', () => {
+    cancelAnimationFrame(LeftStepStopID);
 })
 
 reloadBtn.addEventListener('click', (event) => {
